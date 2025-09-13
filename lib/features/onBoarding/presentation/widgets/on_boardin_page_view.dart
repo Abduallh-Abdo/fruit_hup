60e4c189edc3034e552c8a17ee9f5f66185a13c5
Abduallh-Abdo <ba390690@gmail.com>
2025-09-14 00:43:11 +0300

    use page view item

---
+++ lib/features/onBoarding/presentation/widgets/on_boardin_page_view.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:fruit_hup/core/utils/assets_manager.dart';
+import 'package:fruit_hup/features/onBoarding/presentation/widgets/page_view_item.dart';
+
+class OnBoardinPageView extends StatelessWidget {
+  const OnBoardinPageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      children: const [
+        PageViewItem(
+          backgroundImage: Assets.imagesPageViewItem1BackgroundImage,
+          image: Assets.imagesPageViewItem1Image,
+          subTitle:
+              "اكتشف تجربة تسوق فريدة مع FruitHUB. استكشف مجموعتنا الواسعة من الفواكه الطازجة الممتازة واحصل على أفضل العروض والجودة العالية.",
+          title: Row(
+            children: [Text('مرحبًا بك في '), Text('Fruit'), Text('HUB')],
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/features/onBoarding/presentation/widgets/on_boarding_view_body.dart
+++ lib/features/onBoarding/presentation/widgets/on_boarding_view_body.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:fruit_hup/features/onBoarding/presentation/widgets/on_boardin_page_view.dart';
 
 class OnBoardingViewBody extends StatelessWidget {
   const OnBoardingViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column();
+    return const Column(children: [Expanded(child: OnBoardinPageView())]);
   }
 }
