1daa9acd655c78b96f347d0ab15fa7fef6527fe8
Abduallh-Abdo <ba390690@gmail.com>
2025-09-14 15:20:05 +0300

    add custom button widget

---
+++ lib/core/widgets/custom_button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:fruit_hup/core/utils/app_color.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key, required this.onPressed, required this.text});
+  final void Function() onPressed;
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 54,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: AppColor.primaryColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+        ),
+        onPressed: onPressed,
+        child: Text(text, style: const TextStyle(color: Colors.white)),
+      ),
+    );
+  }
+}
