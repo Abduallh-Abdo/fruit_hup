40f96526f206222e9b6defb6d25da70640b05805
Abduallh-Abdo <ba390690@gmail.com>
2025-09-14 17:07:16 +0300

    add scondary color

--- lib/core/utils/app_color.dart
+++ lib/core/utils/app_color.dart
@@ -4,4 +4,5 @@ import 'package:flutter/material.dart';
 
 abstract class AppColor {
   static const Color primaryColor = Color(0xff1B5E37);
+  static const Color scondrayColor = Color(0xfff4a91f);
 }
