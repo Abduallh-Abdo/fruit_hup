b3d8e7e5a67e45976af7cd360de60f3b88a83274
Abduallh-Abdo <ba390690@gmail.com>
2025-09-14 15:57:01 +0300

    fix pageController.hasClients

--- lib/features/onBoarding/presentation/widgets/on_boardin_page_view.dart
+++ lib/features/onBoarding/presentation/widgets/on_boardin_page_view.dart
@@ -3,28 +3,35 @@ import 'package:fruit_hup/core/utils/assets_manager.dart';
 import 'package:fruit_hup/features/onBoarding/presentation/widgets/page_view_item.dart';
 
 class OnBoardinPageView extends StatelessWidget {
-  const OnBoardinPageView({super.key});
-
+  const OnBoardinPageView({super.key, required this.pageController});
+  final PageController pageController;
   @override
   Widget build(BuildContext context) {
     return PageView(
-      children: const [
+      controller: pageController,
+      children: [
         PageViewItem(
+          isVisible:
+              (pageController.hasClients ? pageController.page!.round() : 0) ==
+              0,
           backgroundImage: Assets.imagesPageViewItem1BackgroundImage,
           image: Assets.imagesPageViewItem1Image,
           subTitle:
               "اكتشف تجربة تسوق فريدة مع FruitHUB. استكشف مجموعتنا الواسعة من الفواكه الطازجة الممتازة واحصل على أفضل العروض والجودة العالية.",
-          title: Row(
+          title: const Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [Text('مرحبًا بك في '), Text('Fruit'), Text('HUB')],
           ),
         ),
         PageViewItem(
+          isVisible:
+              (pageController.hasClients ? pageController.page!.round() : 0) !=
+              0,
           backgroundImage: Assets.imagesPageViewItem2BackgroundImage,
           image: Assets.imagesPageViewItem2Image,
           subTitle:
               "نقدم لك أفضل الفواكه المختارة بعناية. اطلع على التفاصيل والصور والتقييمات لتتأكد من اختيار الفاكهة المثالية",
-          title: Row(
+          title: const Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [Text('ابحث وتسوق')],
           ),
