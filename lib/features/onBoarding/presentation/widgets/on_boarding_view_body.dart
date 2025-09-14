ccdbe2e08a6cff030a8c9463212852a204fe9f31
Abduallh-Abdo <ba390690@gmail.com>
2025-09-14 15:20:50 +0300

    finsh onBoarding  ui

--- lib/features/onBoarding/presentation/widgets/on_boarding_view_body.dart
+++ lib/features/onBoarding/presentation/widgets/on_boarding_view_body.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:fruit_hup/core/utils/app_color.dart';
 import 'package:fruit_hup/features/onBoarding/presentation/widgets/on_boardin_page_view.dart';
 
+import '../../../../core/utils/constants.dart';
+import '../../../../core/widgets/custom_button.dart';
+
 class OnBoardingViewBody extends StatelessWidget {
   const OnBoardingViewBody({super.key});
 
@@ -11,10 +14,19 @@ class OnBoardingViewBody extends StatelessWidget {
     return Column(
       children: [
         Expanded(child: OnBoardinPageView()),
-        DotsIndicator(dotsCount: 2, decorator: DotsDecorator(
-          activeColor: AppColor.primaryColor,
-        color: AppColor.primaryColor.withOpacity(.5),
-        )),
+        DotsIndicator(
+          dotsCount: 2,
+          decorator: DotsDecorator(
+            activeColor: AppColor.primaryColor,
+            color: AppColor.primaryColor.withOpacity(.5),
+          ),
+        ),
+        const SizedBox(height: 29),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: KHorizontalPadding),
+          child: CustomButton(onPressed: () {}, text: 'ابدأ الان'),
+        ),
+        const SizedBox(height: 43),
       ],
     );
   }
