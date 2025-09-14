059247bb1354a8f4c00c42e7053468f38430ea7f
Abduallh-Abdo <ba390690@gmail.com>
2025-09-14 15:25:58 +0300

    add page controller & listener

--- lib/features/onBoarding/presentation/widgets/on_boarding_view_body.dart
+++ lib/features/onBoarding/presentation/widgets/on_boarding_view_body.dart
@@ -6,14 +6,36 @@ import 'package:fruit_hup/features/onBoarding/presentation/widgets/on_boardin_pa
 import '../../../../core/utils/constants.dart';
 import '../../../../core/widgets/custom_button.dart';
 
-class OnBoardingViewBody extends StatelessWidget {
+class OnBoardingViewBody extends StatefulWidget {
   const OnBoardingViewBody({super.key});
+
+  @override
+  State<OnBoardingViewBody> createState() => _OnBoardingViewBodyState();
+}
+
+class _OnBoardingViewBodyState extends State<OnBoardingViewBody> {
+  late PageController _pageController;
+  int currentIndex = 0;
+  @override
+  void initState() {
+    _pageController = PageController();
+    _pageController.addListener(() {
+      currentIndex = _pageController.page!.round();
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Expanded(child: OnBoardinPageView()),
+        const Expanded(child: OnBoardinPageView()),
         DotsIndicator(
           dotsCount: 2,
           decorator: DotsDecorator(
