e1cf17b37df1fdb0def92ed3d5871b074023d59b
Abduallh-Abdo <ba390690@gmail.com>
2025-09-14 17:07:27 +0300

    add text style file

---
+++ lib/core/utils/app_text_styles.dart
@@ -0,0 +1,158 @@
+import 'package:flutter/material.dart';
+
+abstract class AppTextStyles {
+  // ================= Font Sizes =================
+
+  // 48px
+  static const TextStyle regular48 = TextStyle(
+    fontSize: 48,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'cairo',
+  );
+  static const TextStyle semiBold48 = TextStyle(
+    fontSize: 48,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'cairo',
+  );
+  static const TextStyle bold48 = TextStyle(
+    fontSize: 48,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'cairo',
+  );
+
+  // 40px
+  static const TextStyle regular40 = TextStyle(
+    fontSize: 40,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'cairo',
+  );
+  static const TextStyle semiBold40 = TextStyle(
+    fontSize: 40,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'cairo',
+  );
+  static const TextStyle bold40 = TextStyle(
+    fontSize: 40,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'cairo',
+  );
+
+  // 33px
+  static const TextStyle regular33 = TextStyle(
+    fontSize: 33,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'cairo',
+  );
+  static const TextStyle semiBold33 = TextStyle(
+    fontSize: 33,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'cairo',
+  );
+  static const TextStyle bold33 = TextStyle(
+    fontSize: 33,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'cairo',
+  );
+
+  // 28px
+  static const TextStyle regular28 = TextStyle(
+    fontSize: 28,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'cairo',
+  );
+  static const TextStyle semiBold28 = TextStyle(
+    fontSize: 28,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'cairo',
+  );
+  static const TextStyle bold28 = TextStyle(
+    fontSize: 28,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'cairo',
+  );
+
+  // 23px
+  static const TextStyle regular23 = TextStyle(
+    fontSize: 23,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'cairo',
+  );
+  static const TextStyle semiBold23 = TextStyle(
+    fontSize: 23,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'cairo',
+  );
+  static const TextStyle bold23 = TextStyle(
+    fontSize: 23,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'cairo',
+  );
+
+  // 19px
+  static const TextStyle regular19 = TextStyle(
+    fontSize: 19,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'cairo',
+  );
+  static const TextStyle semiBold19 = TextStyle(
+    fontSize: 19,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'cairo',
+  );
+  static const TextStyle bold19 = TextStyle(
+    fontSize: 19,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'cairo',
+  );
+
+  // 16px
+  static const TextStyle regular16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'cairo',
+  );
+  static const TextStyle semiBold16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'cairo',
+  );
+  static const TextStyle bold16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'cairo',
+  );
+
+  // 13px
+  static const TextStyle regular13 = TextStyle(
+    fontSize: 13,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'cairo',
+  );
+  static const TextStyle semiBold13 = TextStyle(
+    fontSize: 13,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'cairo',
+  );
+  static const TextStyle bold13 = TextStyle(
+    fontSize: 13,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'cairo',
+  );
+
+  // 11px
+  static const TextStyle regular11 = TextStyle(
+    fontSize: 11,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'cairo',
+  );
+  static const TextStyle semiBold11 = TextStyle(
+    fontSize: 11,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'cairo',
+  );
+  static const TextStyle bold11 = TextStyle(
+    fontSize: 11,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'cairo',
+  );
+}
