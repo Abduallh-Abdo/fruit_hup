446f6b067db199a383db4b72398254901d2833fa
Abduallh-Abdo <ba390690@gmail.com>
2025-09-15 15:48:16 +0300

    add cutosm button & forget pass & make a email ui

--- lib/features/auth/presentation/widgets/login_view_body.dart
+++ lib/features/auth/presentation/widgets/login_view_body.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:fruit_hup/core/utils/constants.dart';
+import 'package:fruit_hup/core/widgets/custom_button.dart';
 
-import '../../../../core/utils/app_color.dart';
+import '../../../../core/utils/app_colors.dart';
+import '../../../../core/utils/app_text_styles.dart';
 import '../../../../core/widgets/custom_text_form_field.dart';
 
 class LoginViewBody extends StatelessWidget {
@@ -9,24 +11,53 @@ class LoginViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
+    return Padding(
       padding: EdgeInsets.symmetric(horizontal: KHorizontalPadding),
       child: SingleChildScrollView(
         child: Column(
           children: [
-            SizedBox(height: 24),
+            const SizedBox(height: 24),
 
-            CustomTextFormField(
+            const CustomTextFormField(
               hintText: 'البريد الالكتروني',
               textInputType: TextInputType.emailAddress,
             ),
-            SizedBox(height: 16),
-            CustomTextFormField(
+            const SizedBox(height: 16),
+            const CustomTextFormField(
               hintText: 'كلمة المرور',
               textInputType: TextInputType.visiblePassword,
               suffixIcon: Icon(
                 Icons.remove_red_eye,
-                color: AppColor.hintTextColor,
+                color: AppColors.hintTextColor,
+              ),
+            ),
+            const SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  'نسيت كلمة المرور؟',
+                  style: AppTextStyles.semiBold13.copyWith(
+                    color: AppColors.lightPrimaryColor,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 33),
+            CustomButton(onPressed: () {}, text: 'تسجيل الدخول'),
+            const SizedBox(height: 33),
+            Text.rich(
+              TextSpan(
+                text: 'لا تمتلك حساب؟ ',
+                style: AppTextStyles.semiBold16.copyWith(color: Colors.grey),
+                children: [
+                  TextSpan(
+                    text: ' قم بإنشاء حساب',
+                    style: AppTextStyles.semiBold16.copyWith(
+                      color: AppColors.primaryColor,
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
