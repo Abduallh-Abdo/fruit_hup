aa652de20d902c72762b1d7954b0bc652f41b5e5
Abduallh-Abdo <ba390690@gmail.com>
2025-09-16 01:28:27 +0300

    refact dont have account

--- lib/core/widgets/dont_have_account_widget.dart
+++ lib/core/widgets/dont_have_account_widget.dart
@@ -7,21 +7,27 @@ import '../utils/app_colors.dart';
 import '../utils/app_text_styles.dart';
 
 class DontHaveAccountWidget extends StatelessWidget {
-  const DontHaveAccountWidget({super.key});
-
+  const DontHaveAccountWidget({
+    super.key,
+    required this.textAction,
+    required this.text, required this.onTap,
+  });
+  final String textAction;
+  final String text;
+  final void Function() onTap;
   @override
   Widget build(BuildContext context) {
     return Text.rich(
       TextSpan(
-        text: 'لا تمتلك حساب؟ ',
+        text: text,
         style: AppTextStyles.semiBold16.copyWith(color: Colors.grey),
         children: [
+          const TextSpan(text: ' '),
+
           TextSpan(
             recognizer: TapGestureRecognizer()
-              ..onTap = () {
-                context.pushNamed(Routes.signUpView);
-              },
-            text: ' قم بإنشاء حساب',
+              ..onTap = onTap,
+            text: textAction,
             style: AppTextStyles.semiBold16.copyWith(
               color: AppColors.primaryColor,
             ),
