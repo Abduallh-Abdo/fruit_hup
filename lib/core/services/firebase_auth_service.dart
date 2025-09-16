ecb93d832730b6c3b97f2cd669bd9feb82105784
Abduallh-Abdo <ba390690@gmail.com>
2025-09-16 20:06:15 +0300

    add signInWithEmailAndPassword method in FirebaseAuthService

--- lib/core/services/firebase_auth_service.dart
+++ lib/core/services/firebase_auth_service.dart
@@ -21,22 +21,52 @@ class FirebaseAuthService {
         throw CustomException('الكلمة السرية ضعيفة.');
       } else if (e.code == 'email-already-in-use') {
         throw CustomException('البريد الإلكتروني مستخدم بالفعل.');
-      } else if (e.code == 'user-not-found') {
-        throw CustomException(
-          'لم يتم العثور على مستخدم بهذا البريد الإلكتروني.',
-        );
       } else if (e.code == 'network-request-failed') {
         throw CustomException(
           'لا يوجد إتصال بالإنترنت. يرجى التحقق من الاتصال والمحاولة مرة أخرى.',
         );
-      } else if (e.code == 'wrong-password') {
-        throw CustomException('الكلمة السرية غير صحيحة.');
       } else {
         throw CustomException('حدث خطأ. يرجى المحاولة مرة أخرى.');
       }
     } catch (e) {
       log(
         'Exception in AuthRepoImpl.createUserWithEmailAndPassword: $e and code is ${e.hashCode}',
+      );
+
+      throw CustomException('حدث خطأ. يرجى المحاولة مرة أخرى.');
+    }
+  }
+
+  Future<User> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return credential.user!;
+    } on FirebaseAuthException catch (e) {
+      log('Exception in FirebaseAuthService.signInWithEmailAndPassword: $e');
+      if (e.code == 'user-not-found') {
+        throw CustomException(
+          'البريد الإلكتروني او كلمة المرور غير صحيحة. يرجى المحاولة مرة أخرى.',
+        );
+      } else if (e.code == 'wrong-password') {
+        throw CustomException(
+          'البريد الإلكتروني او كلمة المرور غير صحيحة. يرجى المحاولة مرة أخرى.',
+        );
+      } else if (e.code == 'network-request-failed') {
+        throw CustomException(
+          'لا يوجد إتصال بالإنترنت. يرجى التحقق من الاتصال والمحاولة مرة أخرى.',
+        );
+      } else {
+        throw CustomException('حدث خطأ. يرجى المحاولة مرة أخرى.');
+      }
+    } catch (e) {
+      log(
+        'Exception in AuthRepoImpl.signInWithEmailAndPassword: $e and code is ${e.hashCode}',
       );
 
       throw CustomException('حدث خطأ. يرجى المحاولة مرة أخرى.');
