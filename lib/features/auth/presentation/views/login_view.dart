0bf3a4ff9527c195deea6d04b4da68a26624bf5d
Abduallh-Abdo <ba390690@gmail.com>
2025-09-16 20:19:48 +0300

    add login view body bloc consumer

--- lib/features/auth/presentation/views/login_view.dart
+++ lib/features/auth/presentation/views/login_view.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruit_hup/features/auth/presentation/cubits/login_cubit/login_cubit.dart';
-import 'package:fruit_hup/features/auth/presentation/widgets/login_view_body.dart';
 
 import '../../../../core/get_it.dart';
 import '../../../../core/widgets/custom_app_bar.dart';
 import '../../domain/repositories/auth_repo.dart';
+import '../widgets/login_view_body_bloc_consumer.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
@@ -17,7 +17,7 @@ class LoginView extends StatelessWidget {
       child: Scaffold(
         backgroundColor: Colors.white,
         appBar: CustomAppBar(context: context, title: 'تسجيل الدخول'),
-        body: const LoginViewBody(),
+        body: const LoginViewBodyBlocConsumer(),
       ),
     );
   }
---
+++ lib/features/auth/presentation/widgets/login_view_body_bloc_consumer.dart
@@ -0,0 +1,25 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruit_hup/core/widgets/custom_progress_hud.dart';
+import 'package:fruit_hup/features/auth/presentation/widgets/login_view_body.dart';
+import '../cubits/login_cubit/login_cubit.dart';
+
+class LoginViewBodyBlocConsumer extends StatelessWidget {
+  const LoginViewBodyBlocConsumer({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<LoginCubit, LoginState>(
+      listener: (context, state) {
+      },
+      builder: (context, state) {
+        return CustomProgressHud(
+          isLoading: state is LoginLoading ? true : false,
+          child: const LoginViewBody());
+      },
+    );
+  }
+}
