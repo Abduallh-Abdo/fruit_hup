e9dce7d0ff5ae98ee67d8cbaed19c451d477b51c
Abduallh-Abdo <ba390690@gmail.com>
2025-09-16 20:09:06 +0300

    finish login cubit

--- lib/features/auth/presentation/cubits/login_cubit/login_cubit.dart
+++ lib/features/auth/presentation/cubits/login_cubit/login_cubit.dart
@@ -1,10 +1,27 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:fruit_hup/features/auth/domain/repositories/auth_repo.dart';
 
 import '../../../domain/entities/user_entity.dart';
 
 part 'login_state.dart';
 
 class LoginCubit extends Cubit<LoginState> {
-  LoginCubit() : super(LoginInitial());
+  LoginCubit(this.authRepo) : super(LoginInitial());
+  final AuthRepo authRepo;
+  Future<void> signInWithEmailAndPassword(String email, String password) async {
+    emit(LoginLoading());
+    final result = await authRepo.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    result.fold(
+      (failure) {
+        emit(LoginFailure(errorMessage: failure.message));
+      },
+      (user) {
+        emit(LoginSuccess(userEntity: user));
+      },
+    );
+  }
 }
