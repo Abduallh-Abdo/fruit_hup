2811a66d319bf3d7518867ff4334b960ff7d44ad
Abduallh-Abdo <ba390690@gmail.com>
2025-09-16 15:40:29 +0300

    use bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruit_hup/app.dart';
 import 'package:fruit_hup/core/get_it.dart';
 import 'package:fruit_hup/firebase_options.dart';
 
+import 'core/helper/functions/bloc_observer.dart';
 import 'core/services/shared_prefe_singleton.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = AppBlocObserver();
+
   setupGetIt();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await SharedPrefs.init();
