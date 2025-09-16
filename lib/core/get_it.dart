02c0d55dfb4a0932f85bb29a9ae9dd2f989c2ee4
Abduallh-Abdo <ba390690@gmail.com>
2025-09-16 15:05:10 +0300

    add get it file

---
+++ lib/core/get_it.dart
@@ -0,0 +1,13 @@
+import 'package:fruit_hup/core/services/firebase_auth_service.dart';
+import 'package:fruit_hup/features/auth/data/repositories/auth_repo_impl.dart';
+import 'package:fruit_hup/features/auth/domain/repositories/auth_repo.dart';
+import 'package:get_it/get_it.dart';
+
+final sl = GetIt.instance;
+
+void setup() {
+  sl.registerSingleton<FirebaseAuthService>(FirebaseAuthService());
+  sl.registerSingleton<AuthRepo>(
+    AuthRepoImpl(firebaseAuthService: sl<FirebaseAuthService>()),
+  );
+}
