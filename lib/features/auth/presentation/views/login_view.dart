3eac13360ff7bbc47251287784cb4c111a6b53b0
Abduallh-Abdo <ba390690@gmail.com>
2025-09-16 20:11:31 +0300

    provide login cubit

--- lib/features/auth/presentation/views/login_view.dart
+++ lib/features/auth/presentation/views/login_view.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruit_hup/features/auth/presentation/cubits/login_cubit/login_cubit.dart';
 import 'package:fruit_hup/features/auth/presentation/widgets/login_view_body.dart';
 
+import '../../../../core/get_it.dart';
 import '../../../../core/widgets/custom_app_bar.dart';
+import '../../domain/repositories/auth_repo.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: CustomAppBar(context: context, title: 'تسجيل الدخول'),
-      body: const LoginViewBody(),
+    return BlocProvider(
+      create: (context) => LoginCubit(sl.get<AuthRepo>()),
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: CustomAppBar(context: context, title: 'تسجيل الدخول'),
+        body: const LoginViewBody(),
+      ),
     );
   }
 }
