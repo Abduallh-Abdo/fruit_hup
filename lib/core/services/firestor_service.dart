9a47232e4d2a5258e82f331b5de338f3f6ad91d0
Abduallh-Abdo <ba390690@gmail.com>
2025-09-17 12:35:59 +0300

    implement getData

--- lib/core/services/firestor_service.dart
+++ lib/core/services/firestor_service.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fruit_hup/core/services/database_service.dart';
+import 'package:fruit_hup/features/auth/data/models/user_model.dart';
+import 'package:fruit_hup/features/auth/domain/entities/user_entity.dart';
 
 class FireStoreService implements DatabaseService {
   final FirebaseFirestore firestore = FirebaseFirestore.instance;
@@ -9,5 +11,14 @@ class FireStoreService implements DatabaseService {
     required Map<String, dynamic> data,
   }) async {
     await firestore.collection(path).add(data);
+  }
+
+  @override
+  Future<Map<String, dynamic>> getData({
+    required String path,
+    required String documentId,
+  }) async {
+    final userData = await firestore.collection(path).doc(documentId).get();
+    return userData.data()!;
   }
 }
