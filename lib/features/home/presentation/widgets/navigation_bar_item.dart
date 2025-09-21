b0b07c8a71839cc6f31efe7e6a8b1b066b716c99
Abduallh-Abdo <ba390690@gmail.com>
2025-09-21 16:16:50 +0300

    add NavigationNarItem

---
+++ lib/features/home/presentation/widgets/navigation_bar_item.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:fruit_hup/features/home/presentation/widgets/active_item.dart';
+import 'package:fruit_hup/features/home/presentation/widgets/in_active_item.dart';
+
+import '../../../../core/utils/assets_manager.dart';
+import '../../domain/entities/bottom_nav_bar_entity.dart';
+
+class NavigationNarItem extends StatelessWidget {
+  const NavigationNarItem({
+    super.key,
+    required this.isActive,
+    required this.bottomNavBarEntity,
+  });
+  final bool isActive;
+  final BottomNavBarEntity bottomNavBarEntity;
+  @override
+  Widget build(BuildContext context) {
+    return isActive
+        ? const ActiveItem(image: Assets.imagesHomeBold)
+        : InActiveItem(image: bottomNavBarEntity.inactiveImage);
+  }
+}
