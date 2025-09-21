5613ac900fb62798fe73fceb4bbbd7c4681762d9
Abduallh-Abdo <ba390690@gmail.com>
2025-09-21 22:23:58 +0300

    make notification button

---
+++ lib/core/widgets/notification_button.dart
@@ -0,0 +1,23 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../utils/assets_manager.dart';
+
+class NotificationButton extends StatelessWidget {
+  const NotificationButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(12),
+      decoration: const BoxDecoration(
+        color: Color(0xffeef8ed),
+        shape: BoxShape.circle,
+      ),
+      child: SvgPicture.asset(Assets.imagesNotification),
+    );
+  }
+}
