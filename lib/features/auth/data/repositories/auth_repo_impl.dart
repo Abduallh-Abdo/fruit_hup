c726c7ec94a7c82d2f905bf785ec849494334174
Abduallh-Abdo <ba390690@gmail.com>
2025-09-22 00:24:06 +0300

    add saveUserData & impl method

--- lib/features/auth/data/repositories/auth_repo_impl.dart
+++ lib/features/auth/data/repositories/auth_repo_impl.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:developer';
 
 import 'package:dartz/dartz.dart';
@@ -6,11 +7,13 @@ import 'package:fruit_hup/core/errors/exception.dart';
 import 'package:fruit_hup/core/errors/failure.dart';
 import 'package:fruit_hup/core/services/database_service.dart';
 import 'package:fruit_hup/core/utils/backend_endpoint.dart';
+import 'package:fruit_hup/core/utils/constants.dart';
 import 'package:fruit_hup/features/auth/data/models/user_model.dart';
 import 'package:fruit_hup/features/auth/domain/entities/user_entity.dart';
 import 'package:fruit_hup/features/auth/domain/repositories/auth_repo.dart';
 
 import '../../../../core/services/firebase_auth_service.dart';
+import '../../../../core/services/shared_prefe_singleton.dart';
 
 class AuthRepoImpl implements AuthRepo {
   final FirebaseAuthService firebaseAuthService;
@@ -127,7 +130,7 @@ class AuthRepoImpl implements AuthRepo {
   Future addUserData({required UserEntity user}) async {
     await databaseService.addData(
       path: BackendEndpoint.addUserData,
-      data: user.toMap(),
+      data: UserModel.fromEntity(user).toMap(),
       documentId: user.uId,
     );
   }
@@ -139,5 +142,11 @@ class AuthRepoImpl implements AuthRepo {
       documentId: uId,
     );
     return UserModel.fromJson(userData);
+  }
+
+  @override
+  Future saveUserData({required UserEntity user}) async {
+    var jsonData = jsonEncode(UserModel.fromEntity(user).toMap());
+    await SharedPrefs.setString(key: kUserData, value: jsonData);
   }
 }
