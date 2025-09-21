78546ac2b92882196b0bdc6c63f7a7f739e6d772
Abduallh-Abdo <ba390690@gmail.com>
2025-09-21 16:04:10 +0300

    add bottom nav bar entity

---
+++ lib/features/home/domain/entities/bottom_nav_bar_entity.dart
@@ -0,0 +1,35 @@
+import '../../../../core/utils/assets_manager.dart';
+
+class BottomNavBarEntity {
+  final String activeImage, inactiveImage, name;
+
+  BottomNavBarEntity({
+    required this.activeImage,
+    required this.inactiveImage,
+    required this.name,
+  });
+}
+
+
+List<BottomNavBarEntity> listBottomNavBarEntity = [
+  BottomNavBarEntity(
+    activeImage: Assets.imagesHomeBold,
+    inactiveImage: Assets.imagesHomeOutline,
+    name: 'الرئيسية',
+  ),
+  BottomNavBarEntity(
+    activeImage: Assets.imagesProductBold,
+    inactiveImage: Assets.imagesProductOutline,
+    name: 'المنتجات',
+  ),
+  BottomNavBarEntity(
+    activeImage: Assets.imagesShoppingCartBold,
+    inactiveImage: Assets.imagesShoppingCartOutline,
+    name: 'سلة التسوق',
+  ),
+  BottomNavBarEntity(
+    activeImage: Assets.imagesUserBold,
+    inactiveImage: Assets.imagesUserOutline,
+    name: 'حسابي',
+  ),
+];
