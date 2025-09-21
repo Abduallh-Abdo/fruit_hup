76c08a962a11f14dbee36d47c994a5360ee37909
Abduallh-Abdo <ba390690@gmail.com>
2025-09-22 00:22:42 +0300

    add setString method

--- lib/core/services/shared_prefe_singleton.dart
+++ lib/core/services/shared_prefe_singleton.dart
@@ -6,10 +6,15 @@ class SharedPrefs {
     _instance = await SharedPreferences.getInstance();
   }
 
-  static setBool({required String key, required bool value}) {
-   return _instance.setBool(key, value);
+  static setBool({required String key, required bool value}) async {
+    return await _instance.setBool(key, value);
   }
+
   static getBool({required String key}) {
-   return _instance.getBool(key) ?? false;
+    return _instance.getBool(key) ?? false;
+  }
+
+  static setString({required String key, required String value}) async {
+    return await _instance.setString(key, value);
   }
 }
