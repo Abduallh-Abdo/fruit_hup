e59cca63c807f3bc13f50f3136416feac1bc9ff3
Abduallh-Abdo <ba390690@gmail.com>
2025-09-21 16:27:34 +0300

    finsh active item ui

--- lib/features/home/presentation/widgets/active_item.dart
+++ lib/features/home/presentation/widgets/active_item.dart
@@ -1,11 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:fruit_hup/core/utils/app_colors.dart';
+
+import '../../../../core/utils/app_text_styles.dart';
 
 class ActiveItem extends StatelessWidget {
-  const ActiveItem({super.key, required this.image});
-  final String image;
+  const ActiveItem({super.key, required this.image, required this.name});
+  final String image, name;
   @override
   Widget build(BuildContext context) {
-    return SvgPicture.asset(image);
+    return Container(
+      width: 78,
+      height: 30,
+      decoration: ShapeDecoration(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+        color: const Color(0xffeeeeee),
+      ),
+      child: Row(
+        children: [
+          Container(
+            width: 30,
+            height: 30,
+            decoration: ShapeDecoration(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+              color: AppColors.primaryColor,
+            ),
+            child: SvgPicture.asset(image),
+          ),
+          const SizedBox(width: 4),
+          Text(
+            name,
+            style: AppTextStyles.semiBold13.copyWith(
+              color: AppColors.primaryColor,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
