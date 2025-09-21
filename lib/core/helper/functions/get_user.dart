5cf0372834a0601bf29999df4456a69a0def6719
Abduallh-Abdo <ba390690@gmail.com>
2025-09-22 00:32:30 +0300

    add getuser to convert json to user entity

---
+++ lib/core/helper/functions/get_user.dart
@@ -0,0 +1,14 @@
+import 'dart:convert';
+
+import 'package:fruit_hup/core/services/shared_prefe_singleton.dart';
+import 'package:fruit_hup/core/utils/constants.dart';
+import 'package:fruit_hup/features/auth/data/models/user_model.dart';
+import 'package:fruit_hup/features/auth/domain/entities/user_entity.dart';
+
+
+//* convert json to user entity
+UserEntity getUser() {
+  var jsonData = SharedPrefs.getString(key: kUserData);
+  var userEntity = UserModel.fromJson(jsonDecode(jsonData!));
+  return userEntity;
+}
