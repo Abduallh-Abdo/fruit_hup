3c8894aaaaf415bad82c491da78cde16ee41d682
Abduallh Elsaeid <124896053+Abduallh-Abdo@users.noreply.github.com>
2025-09-27 16:13:02 +0300

    add  ProductsRepo

--- lib/core/repos/products_repo/products_repo.dart
+++ lib/core/repos/products_repo/products_repo.dart
@@ -1,1 +1,9 @@
-abstract class ProductsRepo {}
+import 'package:dartz/dartz.dart';
+import 'package:fruit_hup/core/entities/product_entity.dart';
+
+import '../../errors/failure.dart';
+
+abstract class ProductsRepo {
+  Future<Either<Failure, List<ProductEntity>>> getProducts();
+  Future<Either<Failure, List<ProductEntity>>> getBestSellingProducts();
+}
--- lib/core/repos/products_repo/products_repo_impl.dart
+++ lib/core/repos/products_repo/products_repo_impl.dart
@@ -1,5 +1,22 @@
+import 'package:dartz/dartz.dart';
+
+import 'package:fruit_hup/core/entities/product_entity.dart';
+
+import 'package:fruit_hup/core/errors/failure.dart';
+
 import 'products_repo.dart';
 
 class ProductsRepoImpl implements ProductsRepo {
+  @override
+  Future<Either<Failure, List<ProductEntity>>> getBestSellingProducts() {
+    // TODO: implement getBestSellingProducts
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<ProductEntity>>> getProducts() {
+    // TODO: implement getProducts
+    throw UnimplementedError();
+  }
  
 }
