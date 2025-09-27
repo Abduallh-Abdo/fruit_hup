521803e748e0751cc57d6502706aa2a8356c4a60
Abduallh Elsaeid <124896053+Abduallh-Abdo@users.noreply.github.com>
2025-09-28 00:25:35 +0300

    make filter best selling when get data

--- lib/core/get_it.dart
+++ lib/core/get_it.dart
@@ -1,6 +1,6 @@
 import 'package:fruit_hup/core/services/database_service.dart';
 import 'package:fruit_hup/core/services/firebase_auth_service.dart';
-import 'package:fruit_hup/core/services/firestor_service.dart';
+import 'package:fruit_hup/core/services/firestore_service.dart';
 import 'package:fruit_hup/features/auth/data/repositories/auth_repo_impl.dart';
 import 'package:fruit_hup/features/auth/domain/repositories/auth_repo.dart';
 import 'package:get_it/get_it.dart';
--- lib/core/services/firestore_service.dart
+++ lib/core/services/firestore_service.dart
@@ -17,13 +17,29 @@ class FireStoreService implements DatabaseService {
   }
 
   @override
-  Future<dynamic> getData({required String path, String? documentId}) async {
+  Future<dynamic> getData({
+    required String path,
+    String? documentId,
+    Map<String, dynamic>? query,
+  }) async {
     if (documentId != null) {
       final userData = await firestore.collection(path).doc(documentId).get();
       return userData.data();
     } else {
-      final listData = await firestore.collection(path).get();
-      return listData.docs.map((e) => e.data()).toList();
+      Query<Map<String, dynamic>> data = firestore.collection(path);
+      if (query != null) {
+        if (query['orderBy'] != null) {
+          final orderBy = query['orderBy'];
+          final descending = query['descending'];
+          data = data.orderBy(orderBy, descending: descending);
+        }
+        if (query['limit'] != null) {
+          final limit = query['limit'];
+          data = data.limit(limit);
+        }
+      }
+      final result= await data.get();
+      return result.docs.map((e) => e.data()).toList();
     }
   }
 
