320c7aaa5f2541b01a1507dfffa229a93848d6a9
Abduallh Elsaeid <124896053+Abduallh-Abdo@users.noreply.github.com>
2025-09-28 00:00:39 +0300

    impl get data in productrepoimpl

--- lib/core/repos/products_repo/products_repo_impl.dart
+++ lib/core/repos/products_repo/products_repo_impl.dart
@@ -1,12 +1,20 @@
+import 'dart:developer';
+
 import 'package:dartz/dartz.dart';
 
 import 'package:fruit_hup/core/entities/product_entity.dart';
 
 import 'package:fruit_hup/core/errors/failure.dart';
+import 'package:fruit_hup/core/models/product_model.dart';
+import 'package:fruit_hup/core/services/database_service.dart';
 
+import '../../utils/backend_endpoint.dart';
 import 'products_repo.dart';
 
 class ProductsRepoImpl implements ProductsRepo {
+  final DatabaseService databaseService;
+
+  ProductsRepoImpl({required this.databaseService});
   @override
   Future<Either<Failure, List<ProductEntity>>> getBestSellingProducts() {
     // TODO: implement getBestSellingProducts
@@ -14,9 +22,24 @@ class ProductsRepoImpl implements ProductsRepo {
   }
 
   @override
-  Future<Either<Failure, List<ProductEntity>>> getProducts() {
-    // TODO: implement getProducts
-    throw UnimplementedError();
+  Future<Either<Failure, List<ProductEntity>>> getProducts() async {
+    try {
+      final data =
+          await databaseService.getData(path: BackendEndpoint.getProducts)
+              as List<Map<String, dynamic>>;
+
+      List<ProductModel> products = data
+          .map((e) => ProductModel.fromJson(e))
+          .toList();
+      List<ProductEntity> productEntities = products
+          .map((e) => e.toEntity())
+          .toList();
+      return Right(productEntities);
+        //* are the same way as below
+      // return Right(data.map((e) => ProductModel.fromJson(e).toEntity()).toList());
+    } catch (e) {
+      log(e.toString());
+      return const Left(ServerFailure('حدث خطأ. يرجى المحاولة مرة أخرى.'));
+    }
   }
- 
 }
--- lib/core/utils/backend_endpoint.dart
+++ lib/core/utils/backend_endpoint.dart
@@ -2,4 +2,5 @@ abstract class BackendEndpoint {
   static const addUserData='users';
   static const getUserData='users';
   static const isUserExist='users';
+  static const getProducts='products';
 }
