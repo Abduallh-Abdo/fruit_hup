da227b3440cbf8411c3b93c5c333256082e37679
Abduallh Elsaeid <124896053+Abduallh-Abdo@users.noreply.github.com>
2025-09-28 15:09:27 +0300

    remove fileImge & add avgRating to constructor

--- lib/core/entities/product_entity.dart
+++ lib/core/entities/product_entity.dart
@@ -1,6 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'dart:io';
-
 import 'review_entity.dart';
 
 
@@ -9,13 +6,12 @@ class ProductEntity {
   final String code;
   final String description;
   final num price;
-  final File fileImage;
   final bool isFeatured;
   String? imageUrl;
   final int expirationMonths;
   final bool iSOrganic;
   final int numberOfCalories;
-  final num avgRating = 0;
+  final num avgRating ;
   final num ratingCount = 0;
   final int unitAmount;
   final List<ReviewEntity> reviews;
@@ -25,7 +21,6 @@ class ProductEntity {
     required this.code,
     required this.description,
     required this.price,
-    required this.fileImage,
     required this.isFeatured,
     this.imageUrl,
     required this.expirationMonths,
@@ -33,5 +28,6 @@ class ProductEntity {
     required this.numberOfCalories,
     required this.unitAmount,
     required this.reviews,
+    required this.avgRating
   });
 }
