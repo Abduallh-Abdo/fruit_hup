710995cd9b558c376f49f9d3a7aac933d802f6a8
Abduallh Elsaeid <124896053+Abduallh-Abdo@users.noreply.github.com>
2025-10-01 14:02:34 +0300

    add method to CartItemEntity

--- lib/features/home/domain/entities/cart_item_entity.dart
+++ lib/features/home/domain/entities/cart_item_entity.dart
@@ -4,4 +4,10 @@ class CartItemEntity {
   final ProductEntity productEntity;
   int count;
   CartItemEntity({required this.productEntity, this.count = 0});
+
+  num calculateTotalPrice() => productEntity.price * count;
+  num calculateTotalWeight() => productEntity.unitAmount * count;
+
+  increaseCount() => count++;
+  decreaseCount() => count--;
 }
