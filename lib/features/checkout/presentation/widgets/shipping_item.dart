5879280237ba57b894fe3d8eb02c223a650a3c42
Abduallh Elsaeid <124896053+Abduallh-Abdo@users.noreply.github.com>
2025-10-04 18:21:31 +0300

    make ShippingItem onTaped

--- lib/features/checkout/presentation/widgets/shipping_item.dart
+++ lib/features/checkout/presentation/widgets/shipping_item.dart
@@ -11,56 +11,66 @@ class ShippingItem extends StatelessWidget {
     required this.subtitle,
     required this.price,
     required this.isSelected,
+    required this.onTap,
   });
   final String title, subtitle, price;
   final bool isSelected;
+  final VoidCallback onTap;
   @override
   Widget build(BuildContext context) {
-    return AnimatedContainer(
-      duration: const Duration(milliseconds: 300),
-      padding: const EdgeInsets.only(top: 16, bottom: 16, left: 13, right: 28),
-      clipBehavior: Clip.antiAlias,
-      decoration: ShapeDecoration(
-        color: const Color(0x33d9d9d9),
-        shape: RoundedRectangleBorder(
-          side: BorderSide(
-            color: isSelected ? AppColors.primaryColor : Colors.transparent,
-            width: 1,
-          ),
-          borderRadius: BorderRadius.circular(4),
+    return GestureDetector(
+      onTap: onTap,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        padding: const EdgeInsets.only(
+          top: 16,
+          bottom: 16,
+          left: 13,
+          right: 28,
         ),
-      ),
-      child: IntrinsicHeight(
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            isSelected
-                ? const ActiveShippingDots()
-                : const InActiveShippingDots(),
-            const SizedBox(width: 10),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(title, style: AppTextStyles.semiBold13),
-                const SizedBox(height: 6),
-                Text(
-                  subtitle,
-                  style: AppTextStyles.regular13.copyWith(
-                    color: Colors.black.withOpacity(0.5),
+        clipBehavior: Clip.antiAlias,
+        decoration: ShapeDecoration(
+          color: const Color(0x33d9d9d9),
+          shape: RoundedRectangleBorder(
+            side: BorderSide(
+              color: isSelected ? AppColors.primaryColor : Colors.transparent,
+              width: 1,
+            ),
+            borderRadius: BorderRadius.circular(4),
+          ),
+        ),
+        child: IntrinsicHeight(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              isSelected
+                  ? const ActiveShippingDots()
+                  : const InActiveShippingDots(),
+              const SizedBox(width: 10),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(title, style: AppTextStyles.semiBold13),
+                  const SizedBox(height: 6),
+                  Text(
+                    subtitle,
+                    style: AppTextStyles.regular13.copyWith(
+                      color: Colors.black.withOpacity(0.5),
+                    ),
+                  ),
+                ],
+              ),
+              const Spacer(),
+              Center(
+                child: Text(
+                  price,
+                  style: AppTextStyles.bold13.copyWith(
+                    color: const Color(0xff3A8B33),
                   ),
                 ),
-              ],
-            ),
-            const Spacer(),
-            Center(
-              child: Text(
-                price,
-                style: AppTextStyles.bold13.copyWith(
-                  color: const Color(0xff3A8B33),
-                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- lib/features/checkout/presentation/widgets/shipping_section.dart
+++ lib/features/checkout/presentation/widgets/shipping_section.dart
@@ -1,22 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:fruit_hup/features/checkout/presentation/widgets/shipping_item.dart';
 
-class ShippingSection extends StatelessWidget {
+class ShippingSection extends StatefulWidget {
   const ShippingSection({super.key});
 
   @override
+  State<ShippingSection> createState() => _ShippingSectionState();
+}
+
+class _ShippingSectionState extends State<ShippingSection> {
+  int isSelected = -1;
+  @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       children: [
         ShippingItem(
-          isSelected: false,
+          onTap: () {
+            isSelected = 0;
+            setState(() {});
+          },
+          isSelected: isSelected == 0,
           title: 'الدفع عند الاستلام',
           subtitle: 'التسليم من المكان',
           price: '40 جنيه',
         ),
         SizedBox(height: 8),
         ShippingItem(
-          isSelected: true,
+          onTap: () {
+            isSelected = 1;
+            setState(() {});
+          },
+          isSelected: isSelected == 1,
           title: 'الدفع الاونلاين',
           subtitle: 'يرجي تحديد طريقه الدفع',
           price: 'مجاني',
