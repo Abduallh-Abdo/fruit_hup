10a781694a5385657d051315f7153276569956ce
Abduallh Elsaeid <124896053+Abduallh-Abdo@users.noreply.github.com>
2025-10-04 18:16:06 +0300

    add getPages and padding

--- lib/features/checkout/presentation/widgets/checkout_steps_page_view.dart
+++ lib/features/checkout/presentation/widgets/checkout_steps_page_view.dart
@@ -1,24 +1,30 @@
-
 import 'package:flutter/material.dart';
-import 'package:fruit_hup/features/checkout/presentation/widgets/checkout_steps.dart';
+import 'package:fruit_hup/features/checkout/presentation/widgets/shipping_section.dart';
 
 class CheckoutStepsPageView extends StatelessWidget {
-  const CheckoutStepsPageView({
-    super.key,
-    required this.pageController,
-  });
+  const CheckoutStepsPageView({super.key, required this.pageController});
 
   final PageController pageController;
 
   @override
   Widget build(BuildContext context) {
-    return PageView.builder(
-      controller: pageController,
-      physics: const NeverScrollableScrollPhysics(),
-      itemCount: getSteps().length,
-      itemBuilder: (context, index) {
-        return Container();
-      },
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 24),
+      child: PageView.builder(
+        controller: pageController,
+        physics: const NeverScrollableScrollPhysics(),
+        itemCount: getPages().length,
+        itemBuilder: (context, index) {
+          return getPages()[index];
+        },
+      ),
     );
   }
+
+  List<Widget> getPages() => [
+    const ShippingSection(),
+    const ShippingSection(),
+    const ShippingSection(),
+    const ShippingSection(),
+  ];
 }
