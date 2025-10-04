fd815a6d2753206d1bc556f2cc3dea34bbe56bae
Abduallh Elsaeid <124896053+Abduallh-Abdo@users.noreply.github.com>
2025-10-04 18:15:28 +0300

    add ShippingItem

---
+++ lib/features/checkout/presentation/widgets/shipping_item.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:fruit_hup/core/utils/app_colors.dart';
+import 'package:fruit_hup/core/utils/app_text_styles.dart';
+import 'package:fruit_hup/features/checkout/presentation/widgets/active_shipping_dots.dart';
+import 'package:fruit_hup/features/checkout/presentation/widgets/in_active_shipping_dots.dart';
+
+class ShippingItem extends StatelessWidget {
+  const ShippingItem({
+    super.key,
+    required this.title,
+    required this.subtitle,
+    required this.price,
+    required this.isSelected,
+  });
+  final String title, subtitle, price;
+  final bool isSelected;
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      padding: const EdgeInsets.only(top: 16, bottom: 16, left: 13, right: 28),
+      clipBehavior: Clip.antiAlias,
+      decoration: ShapeDecoration(
+        color: const Color(0x33d9d9d9),
+        shape: RoundedRectangleBorder(
+          side: BorderSide(
+            color: isSelected ? AppColors.primaryColor : Colors.transparent,
+            width: 1,
+          ),
+          borderRadius: BorderRadius.circular(4),
+        ),
+      ),
+      child: IntrinsicHeight(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            isSelected
+                ? const ActiveShippingDots()
+                : const InActiveShippingDots(),
+            const SizedBox(width: 10),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(title, style: AppTextStyles.semiBold13),
+                const SizedBox(height: 6),
+                Text(
+                  subtitle,
+                  style: AppTextStyles.regular13.copyWith(
+                    color: Colors.black.withOpacity(0.5),
+                  ),
+                ),
+              ],
+            ),
+            const Spacer(),
+            Center(
+              child: Text(
+                price,
+                style: AppTextStyles.bold13.copyWith(
+                  color: const Color(0xff3A8B33),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
