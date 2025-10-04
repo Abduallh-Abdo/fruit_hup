7bb4dfc03b8114ce4b07951a58bcd2ab5c9291a6
Abduallh Elsaeid <124896053+Abduallh-Abdo@users.noreply.github.com>
2025-10-04 16:01:38 +0300

    use CheckoutStepsPageView & add CustomButton

--- lib/features/checkout/presentation/widgets/checkout_view_body.dart
+++ lib/features/checkout/presentation/widgets/checkout_view_body.dart
@@ -1,15 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:fruit_hup/core/utils/constants.dart';
+import 'package:fruit_hup/core/widgets/custom_button.dart';
 import 'package:fruit_hup/features/checkout/presentation/widgets/checkout_steps.dart';
+import 'package:fruit_hup/features/checkout/presentation/widgets/checkout_steps_page_view.dart';
 
-class CheckoutViewBody extends StatelessWidget {
+class CheckoutViewBody extends StatefulWidget {
   const CheckoutViewBody({super.key});
 
   @override
+  State<CheckoutViewBody> createState() => _CheckoutViewBodyState();
+}
+
+class _CheckoutViewBodyState extends State<CheckoutViewBody> {
+  late PageController pageController;
+
+  @override
+  void initState() {
+    pageController = PageController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: KHorizontalPadding),
-      child: Column(children: [SizedBox(height: 20), CheckoutSteps()]),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: KHorizontalPadding),
+      child: Column(
+        children: [
+          const SizedBox(height: 20),
+          const CheckoutSteps(),
+          Expanded(
+            child: CheckoutStepsPageView(pageController: pageController),
+          ),
+          CustomButton(onPressed: () {}, text: 'التالي'),
+          const SizedBox(height: 32),
+        ],
+      ),
     );
   }
 }
