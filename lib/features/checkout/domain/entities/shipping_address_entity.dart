6f296df3b2d505186a448b787cfa92f99c5ec199
Abduallh Elsaeid <124896053+Abduallh-Abdo@users.noreply.github.com>
2025-10-05 13:53:30 +0300

    add OrderEntity & ShippingAddressEntity

---
+++ lib/features/checkout/domain/entities/shipping_address_entity.dart
@@ -0,0 +1,17 @@
+class ShippingAddressEntity {
+  final String name;
+  final String enail;
+  final String address;
+  final String city;
+  final String addressDetails;
+  final String phone;
+
+  ShippingAddressEntity({
+    required this.name,
+    required this.enail,
+    required this.address,
+    required this.city,
+    required this.addressDetails,
+    required this.phone,
+  });
+}
