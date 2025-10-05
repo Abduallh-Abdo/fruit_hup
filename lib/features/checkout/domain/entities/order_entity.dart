25d10b09bed0f09fb04e3c4e64335533b6674825
Abduallh Elsaeid <124896053+Abduallh-Abdo@users.noreply.github.com>
2025-10-05 14:11:06 +0300

    use cartEntity instead of List CartItemEntity

--- lib/features/checkout/domain/entities/order_entity.dart
+++ lib/features/checkout/domain/entities/order_entity.dart
@@ -1,15 +1,15 @@
 import 'package:fruit_hup/features/checkout/domain/entities/shipping_address_entity.dart';
-import 'package:fruit_hup/features/home/domain/entities/cart_item_entity.dart';
+import 'package:fruit_hup/features/home/domain/entities/cart_entity.dart';
 
 class OrderEntity {
-  final List<CartItemEntity> cartItems;
-  final bool payWithCash;
-  final ShippingAddressEntity shippingAddressEntity;
+  final CartEntity cartEntity;
+  final bool? payWithCash;
+  final ShippingAddressEntity? shippingAddressEntity;
 
   OrderEntity({
-    required this.cartItems,
-    required this.payWithCash,
-    required this.shippingAddressEntity,
+    required this.cartEntity,
+     this.payWithCash,
+     this.shippingAddressEntity,
   });
 }
  
