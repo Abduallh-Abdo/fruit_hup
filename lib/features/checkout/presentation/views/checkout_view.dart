747733a0d5ceb004d8fbfdb1923198e991fc9f2e
Abduallh Elsaeid <124896053+Abduallh-Abdo@users.noreply.github.com>
2025-10-05 14:10:33 +0300

    add provider package

--- lib/features/checkout/presentation/views/checkout_view.dart
+++ lib/features/checkout/presentation/views/checkout_view.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:fruit_hup/core/widgets/build_app_bar.dart';
+import 'package:fruit_hup/features/checkout/domain/entities/order_entity.dart';
 import 'package:fruit_hup/features/checkout/presentation/widgets/checkout_view_body.dart';
+import 'package:fruit_hup/features/home/domain/entities/cart_entity.dart';
+import 'package:provider/provider.dart';
 
 class CheckoutView extends StatelessWidget {
-  const CheckoutView({super.key});
-
+  const CheckoutView({super.key, required this.cartEntity});
+  final  CartEntity cartEntity;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,7 +16,9 @@ class CheckoutView extends StatelessWidget {
         showNotificationButton: false,
         context: context,
       ),
-      body: const CheckoutViewBody(),
+      body: Provider.value(
+         value: OrderEntity(cartEntity: cartEntity),
+        child: const CheckoutViewBody()),
     );
   }
 }
