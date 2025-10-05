cd1183bbf1a685d272ed2070fea8b098c1438cce
Abduallh Elsaeid <124896053+Abduallh-Abdo@users.noreply.github.com>
2025-10-05 23:17:37 +0300

    use onsaved & ValueNotifier &GlobalKey

--- lib/features/checkout/presentation/widgets/address_input_section.dart
+++ lib/features/checkout/presentation/widgets/address_input_section.dart
@@ -1,44 +1,81 @@
 import 'package:flutter/material.dart';
 import 'package:fruit_hup/core/widgets/custom_text_form_field.dart';
+import 'package:fruit_hup/features/checkout/domain/entities/order_entity.dart';
+import 'package:provider/provider.dart';
 
 class AddressInputSection extends StatelessWidget {
-  const AddressInputSection({super.key});
+  const AddressInputSection({
+    super.key,
+    required this.formKey,
+    required this.valueNotifier,
+  });
+  final GlobalKey<FormState> formKey;
+  final ValueNotifier<AutovalidateMode> valueNotifier;
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        CustomTextFormField(
-          hintText: 'الاسم كامل',
-          textInputType: TextInputType.text,
-        ),
-        SizedBox(height: 8),
-        CustomTextFormField(
-          hintText: 'البريد الإلكتروني',
-          textInputType: TextInputType.text,
-        ),
-        SizedBox(height: 8),
-        CustomTextFormField(
-          hintText: 'العنوان',
-          textInputType: TextInputType.text,
-        ),
-        SizedBox(height: 8),
-        CustomTextFormField(
-          hintText: 'المدينه',
-          textInputType: TextInputType.text,
-        ),
-        SizedBox(height: 8),
-        CustomTextFormField(
-          hintText: 'رقم الطابق , رقم الشقه ..',
-          textInputType: TextInputType.text,
-        ),
-        SizedBox(height: 8),
-        CustomTextFormField(
-          hintText: 'رقم الهاتف',
-          textInputType: TextInputType.number,
-        ),
-        SizedBox(height: 8),
-      ],
+    final shippingAddress = context.read<OrderEntity>().shippingAddressEntity;
+    return ValueListenableBuilder<AutovalidateMode>(
+      valueListenable: valueNotifier,
+      builder: (context, value, child) {
+        return Form(
+          key: formKey,
+          autovalidateMode: valueNotifier.value,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                CustomTextFormField(
+                  onSaved: (value) {
+                    shippingAddress!.name = value;
+                  },
+                  hintText: 'الاسم كامل',
+                  textInputType: TextInputType.text,
+                ),
+                const SizedBox(height: 8),
+                CustomTextFormField(
+                  onSaved: (value) {
+                    shippingAddress!.email = value;
+                  },
+                  hintText: 'البريد الإلكتروني',
+                  textInputType: TextInputType.text,
+                ),
+                const SizedBox(height: 8),
+                CustomTextFormField(
+                  onSaved: (value) {
+                    shippingAddress!.address = value;
+                  },
+                  hintText: 'العنوان',
+                  textInputType: TextInputType.text,
+                ),
+                const SizedBox(height: 8),
+                CustomTextFormField(
+                  onSaved: (value) {
+                    shippingAddress!.city = value;
+                  },
+                  hintText: 'المدينه',
+                  textInputType: TextInputType.text,
+                ),
+                const SizedBox(height: 8),
+                CustomTextFormField(
+                  onSaved: (value) {
+                    shippingAddress!.addressDetails = value;
+                  },
+                  hintText: 'رقم الطابق , رقم الشقه ..',
+                  textInputType: TextInputType.text,
+                ),
+                const SizedBox(height: 8),
+                CustomTextFormField(
+                  onSaved: (value) {
+                    shippingAddress!.phone = value;
+                  },
+                  hintText: 'رقم الهاتف',
+                  textInputType: TextInputType.number,
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
