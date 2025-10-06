3f243797d47eec76fdeeb4549534f6bdf99423c0
Abduallh Elsaeid <124896053+Abduallh-Abdo@users.noreply.github.com>
2025-10-06 14:37:11 +0300

    add OrderProductModel

---
+++ lib/features/checkout/data/models/order_product_model.dart
@@ -0,0 +1,36 @@
+import 'package:fruit_hup/features/home/domain/entities/cart_item_entity.dart';
+
+class OrderProductModel {
+  final String code;
+  final String name;
+  final String imageUrl;
+  final double price;
+  final int quantity;
+
+  OrderProductModel({
+    required this.code,
+    required this.name,
+    required this.imageUrl,
+    required this.price,
+    required this.quantity,
+  });
+  factory OrderProductModel.fromEntity(CartItemEntity cartItemEntity) {
+    return OrderProductModel(
+      code: cartItemEntity.productEntity.code,
+      name: cartItemEntity.productEntity.name,
+      imageUrl: cartItemEntity.productEntity.imageUrl!,
+      price: cartItemEntity.productEntity.price.toDouble(),
+      quantity: cartItemEntity.quantity,
+    );
+  }
+
+  toJson(){
+    return {
+      'code': code,
+      'name': name,
+      'imageUrl': imageUrl,
+      'price': price,
+      'quantity': quantity
+    };
+  }
+}
