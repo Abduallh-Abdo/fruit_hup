a24be6f6fbec57b8d86334a08d75829a9f58c004
Abduallh Elsaeid <124896053+Abduallh-Abdo@users.noreply.github.com>
2025-10-11 21:22:56 +0300

    add orderId in OrderModel

--- lib/features/checkout/data/models/order_model.dart
+++ lib/features/checkout/data/models/order_model.dart
@@ -1,6 +1,8 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:fruit_hup/features/checkout/data/models/order_product_model.dart';
 import 'package:fruit_hup/features/checkout/data/models/shipping_address_model.dart';
 import 'package:fruit_hup/features/checkout/domain/entities/order_input_entity.dart';
+import 'package:uuid/uuid.dart';
 
 class OrderModel {
   final String uId;
@@ -8,6 +10,7 @@ class OrderModel {
   final ShippingAddressModel shippingAddressModel;
   final List<OrderProductModel> orderProductModelList;
   final String paymentMethod;
+  final String orderId;
 
   OrderModel({
     required this.uId,
@@ -15,10 +18,12 @@ class OrderModel {
     required this.shippingAddressModel,
     required this.orderProductModelList,
     required this.paymentMethod,
+    required this.orderId,
   });
   factory OrderModel.fromEntity(OrderInputEntity orderEntity) {
     return OrderModel(
       uId: orderEntity.uId,
+      orderId: const Uuid().v4(),
       totalPrice: orderEntity.cartEntity.getTotalPrice(),
       shippingAddressModel: ShippingAddressModel.fromEntity(
         orderEntity.shippingAddressEntity,
@@ -32,6 +37,7 @@ class OrderModel {
   toJson() {
     return {
       'uId': uId,
+      'orderId': orderId,
       'status': 'pending',
       'date': DateTime.now().toString(),
       'totalPrice': totalPrice,
